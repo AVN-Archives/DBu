ecbffaca136a98e938f2980022d0d3b9c387b4c1
Evan Lindsay <evanlindsay@live.com>
2014-05-06 19:43:23 -0700

    Register commands and listeners

--- src/main/java/com/mcprohosting/av/antvenomdbu/AntVenomDBu.java
+++ src/main/java/com/mcprohosting/av/antvenomdbu/AntVenomDBu.java
@@ -1,8 +1,16 @@
 package com.mcprohosting.av.antvenomdbu;
 
+import com.mcprohosting.av.antvenomdbu.commands.GAlert;
+import com.mcprohosting.av.antvenomdbu.commands.GSetMaxPlayers;
 import com.mcprohosting.av.antvenomdbu.config.MainConfig;
+import com.mcprohosting.av.antvenomdbu.listeners.JoinListener;
+import com.mcprohosting.av.antvenomdbu.listeners.MOTDListener;
+import com.mcprohosting.av.antvenomdbu.tasks.NetAlert;
+import com.mcprohosting.av.antvenomdbu.tasks.NetSetMaxPlayers;
+import com.mcprohosting.plugins.dynamicbungee.DynamicBungee;
 import lombok.Getter;
 import net.cubespace.Yamler.Config.InvalidConfigurationException;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class AntVenomDBu extends Plugin {
@@ -16,6 +24,7 @@ public class AntVenomDBu extends Plugin {
     public void onEnable() {
         instance = this;
         this.config = new MainConfig(instance);
+
         try
         {
             this.config.init();
@@ -24,6 +33,22 @@ public class AntVenomDBu extends Plugin {
         {
             e.printStackTrace();
         }
+
+        registerListeners();
+        registerCommands();
+    }
+
+    private void registerCommands() {
+        DynamicBungee.getPlugin().getDispatch().registerTasks(new NetAlert());
+        DynamicBungee.getPlugin().getDispatch().registerTasks(new NetSetMaxPlayers());
+
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new GAlert());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new GSetMaxPlayers());
+    }
+
+    private void registerListeners() {
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new JoinListener());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new MOTDListener());
     }
 
 }
